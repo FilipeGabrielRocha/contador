960f4f6c39edf275d382527f5934b1168c30a195
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-06 19:31:43 -0300

    terminando o projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 const valor = document.getElementById("valor")
 const maisButton = document.getElementById("mais")
-const menusButton = document.getElementById("menos")
+const menosButton = document.getElementById("menos")
 const resetarButton = document.getElementById("resetar")
 
+let contador = 0
+let intervaloId = 0
+
+const atualizacaoValor = () => {
+    valor.innerHTML = contador
+}
+
+maisButton.addEventListener('mousedown', () => {
+    intervaloId = setInterval(() => {
+        contador++
+        atualizacaoValor()
+    }, 100)
+})
+
+menosButton.addEventListener('mousedown', () => {
+    intervaloId = setInterval(() => {
+        contador--
+        atualizacaoValor()
+    }, 100)
+})
+
+resetarButton.addEventListener('click', () => {
+    contador = 0
+    atualizacaoValor()
+})
+
+document.addEventListener('mouseup', () => {
+    clearInterval(intervaloId)
+})
